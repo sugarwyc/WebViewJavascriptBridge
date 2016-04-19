8622a9c89ee892facdef2b2bfe9b44e31f47ff23
王义川 <2978439075@qq.com>
2016-04-19 09:28:16 +0800

    增加carthage支持

---
+++ JavascriptBridge/JavascriptBridge.h
@@ -0,0 +1,21 @@
+//
+//  JavascriptBridge.h
+//  JavascriptBridge
+//
+//  Created by 王义川 on 16/4/19.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JavascriptBridge.
+FOUNDATION_EXPORT double JavascriptBridgeVersionNumber;
+
+//! Project version string for JavascriptBridge.
+FOUNDATION_EXPORT const unsigned char JavascriptBridgeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JavascriptBridge/PublicHeader.h>
+#import <JavascriptBridge/WebViewJavascriptBridge.h>
+#import <JavascriptBridge/WKWebViewJavascriptBridge.h>
+#import <JavascriptBridge/WebViewJavascriptBridge_JS.h>
+#import <JavascriptBridge/WebViewJavascriptBridgeBase.h>
